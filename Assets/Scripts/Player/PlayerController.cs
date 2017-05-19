c46a46b650160a928a7d73d5a9c01c592b61f5e2
JonathanHGS <jonathan-bh29@hotmail.com>
2017-05-19 11:27:25 -0300

    Alteração do pulo.
    O pulo do personagem agora é limitado pelo o uso de raycast.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,40 +6,98 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D playerRigidbody2D;
-
     private float jumpForce;
     private float speed;
+    public bool isJumping;
+    public Transform startLineCast;
+    public Transform endLineCast;
 
-    private bool isJumping;
+    private Animator myAnimator;
+    [SerializeField]
+    private float movementSpeed;
+    private bool facingRight;
+    private bool attack;
 
     // Use this for initialization
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
-        jumpForce = 500;
-        speed = 8;
-        isJumping = false;
+        jumpForce = 400;
 
+        facingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetButtonDown("Jump") && !isJumping)
+        raycast();
+        if (!isJumping)
         {
-            playerRigidbody2D.AddForce(new Vector2(0, jumpForce));
-            isJumping = false;
-            SoundScript.Instance.MakeJumpSound();
-        }
+            if (Input.GetButtonDown("Jump"))
+            {
+                playerRigidbody2D.AddForce(new Vector2(0, jumpForce));
+                isJumping = true;
+            }
+            speed = 8;
+        }else
+            speed = 4;
         transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0) * Time.deltaTime * speed);
 
+        HandleInput();
     }
 
-    void OnCollisionStay2D(Collision2D coll)
+    private void raycast()
     {
-        if (coll.gameObject.tag == "Ground")
+        if (Physics2D.Linecast(startLineCast.position,endLineCast.position))
+        {
             isJumping = false;
+        }
+        else
+        {
+            isJumping = true;
+        }
+    }
+
+    void Fixedupdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * movementSpeed;
+        HandleMovement(horizontal);
+        Flip(horizontal);
+        HandleAttacks();
+    }
+    private void HandleMovement(float horizontal)
+    {
+        playerRigidbody2D.velocity = new Vector2(horizontal * movementSpeed, playerRigidbody2D.velocity.y);
+    }
+    private void HandleAttacks()
+    {
+        if (attack)
+        {
+            myAnimator.SetTrigger("attack");
+        }
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            attack = true;
+        }
+    }
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+
+    }
+    private void ResetValues()
+    {
+        attack = false;
     }
 
 }
